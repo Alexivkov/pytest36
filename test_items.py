62d6bc04376536015f138c9146044e017788ec0b
Alexivkov <sivkov@inbox.ru>
2019-12-17 14:23:40 +0500

    Add assert in test_items file

--- test_items.py
+++ test_items.py
@@ -5,4 +5,5 @@ link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_guest_should_see_login_link(browser):
     browser.get(link)
     time.sleep(4)
-    browser.find_element_by_css_selector("#add_to_basket_form button")
+    button = browser.find_element_by_css_selector("#add_to_basket_form button")
+    assert button.text != ''
